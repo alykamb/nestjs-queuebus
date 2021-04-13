5e6e37a7bc4b4568d37d6790e7d02e32ec7adc18
Alykam Burdzaki <alykam@gmail.com>
2021-04-13 15:16:34 -0300

    remove unused interface

--- src/interfaces/queues/queueJob.interface.ts
+++
@@ -1,7 +0,0 @@
-import { IExecutionOptions } from '../executionOptions.interface'
-import { IQueue } from './queue.interface'
-
-export interface IQueueJob<T = IQueue> {
-    job: T
-    options: IExecutionOptions
-}
